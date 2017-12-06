fba4c3b9dadb1bfeac15694f50567235e23ca988
mauwahid <mau.wahid@gmail.com>
2017-12-06 17:23:51 +0700

    Astri Hotel Availability search

--- src/main/java/com/mauwahid/tm/travelmgt/repository/api/astrindo/AstriHotelAvailability.java
+++ src/main/java/com/mauwahid/tm/travelmgt/repository/api/astrindo/AstriHotelAvailability.java
@@ -61,6 +61,7 @@ public class AstriHotelAvailability implements HotelSearchInterface {
         hotelHotel.setStatusCode(StatusCode.ERROR_API+"");
         hotelHotel.setStatusDesc(StatusCode.S_ERROR_API+" : "+ex.toString());
 
+//        logErrorHelper = new LogErrorHelper();
         logErrorHelper.saveErrorExc(ApiStatic.API_HOTEL_SEARCH, ex.toString(), param, jsonData);
 
         return hotelHotels;
