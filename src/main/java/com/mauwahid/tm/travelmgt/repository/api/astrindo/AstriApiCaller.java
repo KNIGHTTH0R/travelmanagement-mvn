5c30b34879ad50d4c138d0cb400c8434e23a8e3f
mauwahid <mau.wahid@gmail.com>
2017-12-22 10:53:20 +0700

    jj

--- src/main/java/com/mauwahid/tm/travelmgt/repository/api/astrindo/AstriApiCaller.java
+++ src/main/java/com/mauwahid/tm/travelmgt/repository/api/astrindo/AstriApiCaller.java
@@ -23,17 +23,17 @@ public class AstriApiCaller {
 
     /* production */
     //production
-  //  public static String ORGCODE = "wrOjkduaDaoQQ07t1lVgJw==";
+    public static String ORGCODE = "wrOjkduaDaoQQ07t1lVgJw==";
 
-  //  public static String MERCHANTID = "PiOw9Qi92FX0rVEPKZvuZA==";
+    public static String MERCHANTID = "PiOw9Qi92FX0rVEPKZvuZA==";
 
-  //  public static String USERNAME = "+qggfittARzhpcW5pZW83A==";
+    public static String USERNAME = "+qggfittARzhpcW5pZW83A==";
 
-  //  public static String PASSWORD = "wqg/LsX2Z+Ok/8zaKLXKNg==";
+    public static String PASSWORD = "wqg/LsX2Z+Ok/8zaKLXKNg==";
 
 
 
-   /* dev */
+   /* dev
 
     public static String ORGCODE = "DHWJddDxgJtZXcpYqA5uTg==";
 
@@ -43,7 +43,7 @@ public class AstriApiCaller {
 
 
     public static String PASSWORD = "mtTsoQWJuz5AOu+F+2Og1g9yInDvhxegiA7PiHgLyTzPI41kFjOVWNsFCU9tmtQ2";
-
+*/
 
     public static String callApiPost(String uri, Map<String, String> params) throws IOException {
         PostStdRequester postStdRequester = new PostStdRequester();
